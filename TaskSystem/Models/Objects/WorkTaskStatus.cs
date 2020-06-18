8ca94267bd4dbbdbdebeb5f3a5c8022ace98668c
Shevelev.MS <shevelev.ms@dns-shop.ru>
2020-06-18 11:03:41 +1000

    Status Description

--- TaskSystem/Models/Objects/WorkTaskStatus.cs
+++ TaskSystem/Models/Objects/WorkTaskStatus.cs
@@ -10,10 +10,25 @@ namespace TaskSystem.Models.Objects
     /// </summary>
     public enum WorkTaskStatus : byte
     {
+        /// <summary>
+        /// Статус нового задания
+        /// </summary>
         New = 1,
+        /// <summary>
+        /// Статус задания, находящегося в работе
+        /// </summary>
         InWork = 2,
+        /// <summary>
+        /// Статус приостановленного исполнителем задания
+        /// </summary>
         Paused = 3,
+        /// <summary>
+        /// Статус выполненного исполнителем задания
+        /// </summary>
         Completed = 4,
+        /// <summary>
+        /// Статус отмененного задания создателем или исполнителем
+        /// </summary>
         Canceled = 5
     }
 }

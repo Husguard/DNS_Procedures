18d8bd693f8caa60eed854e093e56c94791d88f3
Shevelev.MS <shevelev.ms@dns-shop.ru>
2020-06-18 18:15:12 +1000

    Добавлено описание

--- TaskSystem/Controllers/CommentController.cs
+++ TaskSystem/Controllers/CommentController.cs
@@ -24,16 +24,25 @@ namespace TaskSystem.Controllers
         }
 
         /// <summary>
-        /// Получение всех работников
+        /// Получение всех комментариев определенного задания
         /// </summary>
+        /// <param name="taskId">Идентификатор задания</param>
         [HttpGet]
         [Route("GetCommentsOfTask")]
         public ServiceResponseGeneric<IEnumerable<CommentDto>> GetCommentsOfTask(int taskId) => _commentService.GetCommentsOfTask(taskId);
 
+        /// <summary>
+        /// Получение всех комментариев определенного работника
+        /// </summary>
+        /// <param name="employeeId">Идентификатор работника</param>
         [HttpGet]
         [Route("GetCommentsOfEmployee")]
         public ServiceResponseGeneric<IEnumerable<CommentDto>> GetCommentsOfEmployee(int employeeId) => _commentService.GetCommentsOfEmployee(employeeId);
-
+        
+        /// <summary>
+        /// Добавление комментария к заданию
+        /// </summary>
+        /// <param name="commentDto">Данные комментария</param>
         [HttpPost]
         [Route("AddCommentToTask")]
         public ServiceResponse AddCommentToTask(CommentDto commentDto) => _commentService.AddCommentToTask(commentDto);
--- TaskSystem/Controllers/TaskController.cs
+++ TaskSystem/Controllers/TaskController.cs
@@ -39,31 +39,55 @@ namespace TaskSystem.Controllers
         [Route("GetTaskByID")]
         public ServiceResponseGeneric<IEnumerable<WorkTaskDto>> GetTaskByID(int taskId) => _taskService.GetTaskByID(taskId);
 
+        /// <summary>
+        /// Получение последних версий всех заданий, у которых выбранный статус
+        /// </summary>
+        /// <param name="statusId">Статус задания</param>
         [HttpGet]
         [Route("GetTasksByStatus")]
-        public ServiceResponseGeneric<IEnumerable<WorkTaskDto>> GetTasksByStatus(WorkTaskStatus statusId) => _taskService.GetTasksByStatus(statusId); // проблема в работе процедуры
-        
+        public ServiceResponseGeneric<IEnumerable<WorkTaskDto>> GetTasksByStatus(WorkTaskStatus statusId) => _taskService.GetTasksByStatus(statusId); 
+
+        /// <summary>
+        /// Получение последней версии определенного задания
+        /// </summary>
+        /// <param name="taskId">Идентификатор задания</param>
         [HttpGet]
         [Route("GetLastVersionOfTask")]
-        public ServiceResponseGeneric<WorkTaskDto> GetLastVersionOfTask(int taskId) => _taskService.GetLastVersionOfTask(taskId); // проблема в работе процедуры
+        public ServiceResponseGeneric<WorkTaskDto> GetLastVersionOfTask(int taskId) => _taskService.GetLastVersionOfTask(taskId);
 
-
+        /// <summary>
+        /// Получение списка заданий, у которых определенный исполнитель
+        /// </summary>
+        /// <param name="performerId">Идентификатор исполнителя</param>
         [HttpGet]
         [Route("GetTasksByPerformer")]
         public ServiceResponseGeneric<IEnumerable<WorkTaskDto>> GetTasksByPerformer(int performerId) => _taskService.GetTasksByPerformer(performerId);
 
+        /// <summary>
+        /// Получение списка заданий, у которых определенный создатель
+        /// </summary>
+        /// <param name="creatorId">Идентификатор создателя</param>
         [HttpGet]
         [Route("GetTasksByCreator")]
         public ServiceResponseGeneric<IEnumerable<WorkTaskDto>> GetTasksByCreator(int creatorId) => _taskService.GetTasksByCreator(creatorId);
 
+        /// <summary>
+        /// Добавление нового задания
+        /// </summary>
+        /// <param name="workTask">Данные нового задания</param>
         [HttpPost]
         [Route("AddNewTask")]
         public ServiceResponse AddNewTask(WorkTaskDto workTask) => _taskService.AddNewTask(workTask);
 
+        /// <summary>
+        /// Добавление новой версии определенного задания
+        /// </summary>
+        /// <param name="moneyAward">Новая денежная награда</param>
+        /// <param name="statusId">Новый статус задания</param>
+        /// <param name="taskId">Идентификатор задания</param>
         [HttpPost]
         [Route("AddTaskVersion")]
-        public ServiceResponse AddTaskVersion(decimal moneyAward, WorkTaskStatus statusId, int taskId) => _taskService.AddTaskVersion(moneyAward, statusId, taskId);  // проблема в работе процедуры
-
+        public ServiceResponse AddTaskVersion(decimal moneyAward, WorkTaskStatus statusId, int taskId) => _taskService.AddTaskVersion(moneyAward, statusId, taskId);
 
     }
 }
